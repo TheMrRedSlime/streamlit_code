748672d6a8ade58607dbf8935123ba5f39f70954
TheMrRedSlime <69750444+TheMrRedSlime@users.noreply.github.com>
2025-09-20 10:05:58 +0530

    Project new 1

--- project.py
+++ project.py
@@ -1,51 +1,76 @@
-import pandas as pd
-from bs4 import BeautifulSoup
 import streamlit as st
-from io import BytesIO
-import requests
+import networkx as nx
+import matplotlib.pyplot as plt
 
-def scrape_books():
-    url="https://books.toscrape.com/"
-    response=requests.get(url)
-    soup = BeautifulSoup(response.text,'html.parser')
+st.title("Path compare")
 
-    #print(soup)
-    books=[]
+cities = [
+    "Delhi", "Mumbai", "Kolkata", "Chennai", "Bangalore",
+    "Hyderabad", "Pune", "Ahmedabad", "Jaipur", "Lucknow",
+    "Bhopal", "Nagpur", "Patna", "Chandigarh", "Guwahati"
+]
+
+st.subheader("City #1")
+city1 = st.selectbox(options=cities, label="City #1")
+
+st.subheader("City #1")
+city2 = st.selectbox(options=cities, label="City #2")
+
+
+if st.button("Compare"):
+    if city1 is None or city2 is None:
+        st.error("Please choose a city!")
+    else:
     
-    for article in soup.find_all('article', class_="product_pod"):
-        title = article.h3.a["title"]
-        price = article.find("div", class_="product_price").find("p", class_="price_color").text
-        availability = article.find("div", class_="product_price").find("p", class_="instock availability").text.strip()
-        books.append({
-            "Title": title,
-            "Price": price,
-            "available": availability
-        })
-    return books
+        g=nx.Graph()
+        g.add_nodes_from(cities)
+        
+        # Routes with approximate distances (in km)
+        g.add_edge("Delhi", "Jaipur", weight=280)
+        g.add_edge("Delhi", "Lucknow", weight=500)
+        g.add_edge("Delhi", "Chandigarh", weight=250)
+        g.add_edge("Delhi", "Bhopal", weight=760)
+        
+        g.add_edge("Jaipur", "Ahmedabad", weight=670)
+        g.add_edge("Jaipur", "Mumbai", weight=1140)
+        
+        g.add_edge("Ahmedabad", "Mumbai", weight=530)
+        g.add_edge("Mumbai", "Pune", weight=150)
+        g.add_edge("Mumbai", "Hyderabad", weight=710)
+        g.add_edge("Mumbai", "Nagpur", weight=820)
+        
+        g.add_edge("Hyderabad", "Bangalore", weight=570)
+        g.add_edge("Hyderabad", "Chennai", weight=630)
+        g.add_edge("Hyderabad", "Nagpur", weight=500)
+        
+        g.add_edge("Bangalore", "Chennai", weight=340)
+        g.add_edge("Bangalore", "Pune", weight=840)
+        
+        g.add_edge("Kolkata", "Patna", weight=590)
+        g.add_edge("Kolkata", "Bhubaneswar", weight=440)
+        g.add_edge("Kolkata", "Guwahati", weight=1030)
+        g.add_edge("Kolkata", "Delhi", weight=1500)
+        
+        g.add_edge("Nagpur", "Bhopal", weight=350)
+        g.add_edge("Lucknow", "Patna", weight=530)
+        g.add_edge("Patna", "Delhi", weight=1080)
 
-st.header("Book scraping")
-
-if st.button("Scrape books"):
-    with st.spinner("Getting book data..."):
-
-        df=scrape_books()
-        st.dataframe(df)
-        st.success("Successfully scraped books!")
-        # Convert DataFrame to CSV
-        csv_buffer = BytesIO()
-        # Create an in-memory buffer for binary data. We’ll write the CSV there instead of writing a file to disk.
-        df = pd.DataFrame(df)
-        df.to_csv(csv_buffer, index=False)
-        # Write the DataFrame to the BytesIO buffer in CSV format. index=False tells pandas not to write the DataFrame index
-        # as a separate column (keeps file neat).
-        # pandas will encode the CSV text into bytes when writing to a BytesIO.
-        csv_data = csv_buffer.getvalue()
-        # Get the contents of the buffer as raw bytes — ready to pass to st.download_button (which accepts bytes, str, or file-like objects).
-
-        # Download button
-        st.download_button(
-            label="📥 Download CSV",
-            data=csv_data,
-            file_name="books_data.csv",
-            mime="text/csv"
+        pos=nx.spring_layout(g)
+        nx.draw(
+            g, pos, with_labels=True, node_size=1000,
+            node_color="green", font_color="black", edgecolors="black", font_size=12
         )
+
+        path=nx.shortest_path(g, source=city1, target=city2)
+        length=nx.shortest_path_length(g, source=city1, target=city2, weight="weight")
+
+        st.info(f"Shortest path: {" -> ".join(path)}")
+        st.info(f"Shortest length: {length}km")
+
+        path_edges = list(zip(path, path[1:]))
+
+        nx.draw_networkx_edges(g, pos, edgelist=path_edges, width=3, edge_color="blue")
+        
+        plt.gca().set_facecolor("grey")
+        #plt.figure(facecolor="grey",figure=plt.figure())
+        st.pyplot(plt)
