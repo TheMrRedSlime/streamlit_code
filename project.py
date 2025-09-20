3b0891c75a35128a0482155bd29b29b08317d9a2
TheMrRedSlime <69750444+TheMrRedSlime@users.noreply.github.com>
2025-09-20 11:22:15 +0530

    pos fix

--- project.py
+++ project.py
@@ -2,7 +2,9 @@ import streamlit as st
 import networkx as nx
 import matplotlib.pyplot as plt
 
-st.title("Path compare")
+st.title("Route Finder")
+st.subheader("Find the shortest path between towns!")
+
 
 cities = [
     "Delhi", "Mumbai", "Kolkata", "Chennai", "Bangalore",
@@ -10,10 +12,8 @@ cities = [
     "Bhopal", "Nagpur", "Patna", "Chandigarh", "Guwahati"
 ]
 
-st.subheader("City #1")
 city1 = st.selectbox(options=cities, label="City #1")
 
-st.subheader("City #1")
 city2 = st.selectbox(options=cities, label="City #2")
 
 
@@ -47,7 +47,7 @@ if st.button("Compare"):
         g.add_edge("Bangalore", "Pune", weight=840)
         
         g.add_edge("Kolkata", "Patna", weight=590)
-        g.add_edge("Kolkata", "Bhubaneswar", weight=440)
+        #g.add_edge("Kolkata", "Bhubaneswar", weight=440)
         g.add_edge("Kolkata", "Guwahati", weight=1030)
         g.add_edge("Kolkata", "Delhi", weight=1500)
         
@@ -55,9 +55,28 @@ if st.button("Compare"):
         g.add_edge("Lucknow", "Patna", weight=530)
         g.add_edge("Patna", "Delhi", weight=1080)
 
-        pos=nx.spring_layout(g)
+        pos = {
+            "Delhi": (0.5, 0.8),
+            "Chandigarh": (0.45, 0.9),
+            "Jaipur": (0.4, 0.75),
+            "Lucknow": (0.65, 0.75),
+            "Bhopal": (0.5, 0.6),
+            "Nagpur": (0.55, 0.5),
+            "Ahmedabad": (0.35, 0.65),
+            "Mumbai": (0.3, 0.5),
+            "Pune": (0.3, 0.45),
+            "Hyderabad": (0.5, 0.4),
+            "Bangalore": (0.45, 0.25),
+            "Chennai": (0.55, 0.2),
+            "Kolkata": (0.85, 0.65),
+            "Patna": (0.75, 0.7),
+            "Guwahati": (0.95, 0.8),
+        }
+        fig, ax = plt.subplots(facecolor='grey')
+        ax.set_facecolor('lightgrey')
+        
         nx.draw(
-            g, pos, with_labels=True, node_size=1000,
+            g, pos, ax=ax, with_labels=True, node_size=1000,
             node_color="green", font_color="black", edgecolors="black", font_size=12
         )
 
@@ -71,6 +90,4 @@ if st.button("Compare"):
 
         nx.draw_networkx_edges(g, pos, edgelist=path_edges, width=3, edge_color="blue")
         
-        plt.gca().set_facecolor("grey")
-        #plt.figure(facecolor="grey",figure=plt.figure())
-        st.pyplot(plt)
+        st.pyplot(fig)
